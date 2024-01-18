85247f126d986d1cfce48e5a7ee06b9470f41367
lauracercas <laura.cercas@edu.uah.es>
2024-01-18 20:01:12 +0100

    integration test with DataJpaTest

--- src/test/java/com/lauracercas/moviecards/integrationtest/repositories/ActorJPAIT.java
+++ src/test/java/com/lauracercas/moviecards/integrationtest/repositories/ActorJPAIT.java
@@ -1,42 +1,36 @@
 package com.lauracercas.moviecards.integrationtest.repositories;
 
-import com.lauracercas.moviecards.integrationtest.SpringTest;
 import com.lauracercas.moviecards.model.Actor;
 import com.lauracercas.moviecards.repositories.ActorJPA;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ActorJPAIT extends SpringTest {
+@DataJpaTest
+public class ActorJPAIT {
+    @Autowired
+    private TestEntityManager entityManager;
 
+    @Autowired
+    private ActorJPA actorJPA;
 
- @Test
- @Transactional @Rollback
- public void pruebaConexion(){
- assertTrue(session().isConnected());
- }
+    @Test
+    public void testCreateActor() {
+        // given
+        Actor actor = new Actor();
+        actor.setName("actor");
+        actor.setBirthDate(new Date());
 
- @Test
- @Transactional
- public void testCreateActor() {
- Actor actor = new Actor();
- actor.setName("actor");
- actor.setBirthDate(new Date());
+        // when
+        actorJPA.save(actor);
 
- session().persist(actor);
- session().flush();
-
- Actor foundActor = session().find(Actor.class, actor.getId());
-
- assertEquals(actor, foundActor);
- }
-
- }
+        // then
+        Actor foundActor = entityManager.find(Actor.class, actor.getId());
+        assertEquals(actor, foundActor);
+    }
+}
