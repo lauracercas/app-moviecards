25bf36fc922d1672a60668324305ab6335e6de23
lauracercas <laura.cercas@edu.uah.es>
2024-02-21 19:21:17 +0100

    try set java 11

--- src/main/java/com/lauracercas/moviecards/model/Actor.java
+++ src/main/java/com/lauracercas/moviecards/model/Actor.java
@@ -1,6 +1,6 @@
 package com.lauracercas.moviecards.model;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
--- src/main/java/com/lauracercas/moviecards/model/Movie.java
+++ src/main/java/com/lauracercas/moviecards/model/Movie.java
@@ -1,7 +1,7 @@
 package com.lauracercas.moviecards.model;
 
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 
 import java.util.List;
 import java.util.Objects;
